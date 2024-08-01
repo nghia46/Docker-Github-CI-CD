b6bd33fbbda987f8ad05795794c8ec16a1cedc0d
nghia46 <nghialoe46@gmail.com>
2024-08-01 10:45:43 +0700

    edit

--- Docker-Ci-Di/Controllers/WeatherForecastController.cs
+++ Docker-Ci-Di/Controllers/WeatherForecastController.cs
@@ -24,15 +24,15 @@ namespace Docker_Ci_Di.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-100, 100),
+                TemperatureC = Random.Shared.Next(-1000, 1000),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
-        [HttpGet(Name = "Greetings")]
-        public string GetGreetings()
+        [HttpGet("Greetings")]
+        public IActionResult GetGreetings()
         {
-            return "Hello user!";
+            return Ok("Hello user!");
         }
     }
 }
